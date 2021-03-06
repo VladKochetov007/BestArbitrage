5b24a76bfadb8597fd73dae986f3574d369b3672
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2021-03-06 13:57:07 +0200

    py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 import setuptools
 
-with open('../copy/README.md') as file:
+with open('./README.md') as file:
     long_desc = file.read()
 
 setup(
@@ -11,8 +11,8 @@ setup(
     author='Vlad Kochetov',
     author_email='vladyslavdrrragonkoch@gmail.com',
     packages=setuptools.find_packages(),
-    version='4.1',
-    description='Trading system for stocks, forex and others',
+    version='0.3.2',
+    description='Arbitrage cryptocurrency smartly',
     long_description=long_desc,
     long_description_content_type="text/markdown",
     url='https://github.com/VladKochetov007/BestArbitrage',
